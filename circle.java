f26159d2ec1976aecc9f2475f43e7979d71c90b9
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:27:30 +0530

    Circle.java

---
+++ circle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class circle
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int r;
+double area;
+System.out.println("Enter the radius");
+r=sc.nextInt();
+area=3.14*r*r;
+System.out.println("area="+area);
+}
+}
