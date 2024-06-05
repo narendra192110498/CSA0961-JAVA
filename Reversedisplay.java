e9e80fe91ee2ce25790999c4e53033454b639e3a
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:19:32 +0530

    Reversedisplay.java

---
+++ Reversedisplay.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Reversedisplay
+{
+	public static void main(String args[])
+	{
+		
+		for(int i=9;i>=0;i--)
+		{
+			System.out.println("display numbers="+i);
+		}
+	}
+}
