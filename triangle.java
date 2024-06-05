4d7f31fb27640cebd137ec70f84f237da360f3df
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:50:30 +0530

    triangle.java

---
+++ triangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class triangle
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int b,h;
+double area;
+System.out.println("Enter the base");
+b=sc.nextInt();
+System.out.println("enter the height");
+h=sc.nextInt();
+area=0.5*b*h;
+System.out.println("area of triangle="+area);
+}
+}
