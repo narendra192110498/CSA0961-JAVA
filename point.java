76333824c796cec4e63d93cc546fb731f956622d
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:44:01 +0530

    point.java

---
+++ point.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class point
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double s,c,f,n1;
+System.out.println("Enter the 1 num");
+n1=sc.nextInt();
+s=n1*n1;
+c=n1*n1*n1;
+f=n1*n1*n1*n1;
+System.out.println("square="+s+"cube="+c+"fourthpower="+f);
+}
+}
