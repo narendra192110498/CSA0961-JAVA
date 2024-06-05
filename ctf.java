4c404982d83795222cbf3c4e2a9a0efab73f76fb
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:29:16 +0530

    ctf.java

---
+++ ctf.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ctf
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double c,f;
+System.out.println("Enter the celsius");
+c=sc.nextInt();
+f=1.8*c+32;
+System.out.println("converting elsius to farenheit="+f);
+}
+}
