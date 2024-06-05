d918ddbedf4153c4542dd7df5ea7cdd630e43b25
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:39:01 +0530

    itm.java

---
+++ itm.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class itm
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double inch,meter;
+System.out.println("Enter the farenheit");
+inch=sc.nextInt();
+meter=inch*0.0254;
+System.out.println("converting inch to meter="+meter);
+}
+}
