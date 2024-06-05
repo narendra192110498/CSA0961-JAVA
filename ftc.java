1333e006ca3518a4aeb69ee9aa203ac54b296f35
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:31:53 +0530

    ftc.java

---
+++ ftc.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ftc
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double c,f;
+System.out.println("Enter the farenheit");
+f=sc.nextInt();
+c=(f-32)/1.8;
+System.out.println("converting farenheit t celsius="+c);
+}
+}
