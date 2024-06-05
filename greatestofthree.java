73b186106690347c54aaa8ff283c07331cf24b74
Narendra Reddy <112068278+narendra192110498@users.noreply.github.com>
2024-06-05 11:34:30 +0530

    greatestofthree.java

---
+++ greatestofthree.java
@@ -0,0 +1,10 @@
+public class greatestofthree{
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 15;
+        int greatest = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
+        
+        System.out.println("The greatest of the three numbers is: " + greatest);
+    }
+}
